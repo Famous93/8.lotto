b93ca058044e84e0a84e8be4ac83e1c0412403f4
Famous93 <73938606+Famous93@users.noreply.github.com>
2021-05-10 17:17:30 +0300

    Add project files.

---
+++ array lotto/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace array_lotto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.mynum1 = new System.Windows.Forms.TextBox();
+            this.mynum2 = new System.Windows.Forms.TextBox();
+            this.mynum3 = new System.Windows.Forms.TextBox();
+            this.mynum4 = new System.Windows.Forms.TextBox();
+            this.mynum5 = new System.Windows.Forms.TextBox();
+            this.mynum6 = new System.Windows.Forms.TextBox();
+            this.mynum7 = new System.Windows.Forms.TextBox();
+            this.mynum8 = new System.Windows.Forms.TextBox();
+            this.mynum9 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Tarkista = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.num1.Location = new System.Drawing.Point(124, 56);
+            this.num1.Margin = new System.Windows.Forms.Padding(3);
+            this.num1.Name = "num1";
+            this.num1.Padding = new System.Windows.Forms.Padding(2);
+            this.num1.Size = new System.Drawing.Size(274, 20);
+            this.num1.TabIndex = 8;
+            this.num1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(527, 44);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Generate numbers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mynum1
+            // 
+            this.mynum1.Location = new System.Drawing.Point(12, 247);
+            this.mynum1.Name = "mynum1";
+            this.mynum1.Size = new System.Drawing.Size(50, 20);
+            this.mynum1.TabIndex = 18;
+            // 
+            // mynum2
+            // 
+            this.mynum2.Location = new System.Drawing.Point(68, 247);
+            this.mynum2.Name = "mynum2";
+            this.mynum2.Size = new System.Drawing.Size(50, 20);
+            this.mynum2.TabIndex = 19;
+            // 
+            // mynum3
+            // 
+            this.mynum3.Location = new System.Drawing.Point(124, 247);
+            this.mynum3.Name = "mynum3";
+            this.mynum3.Size = new System.Drawing.Size(50, 20);
+            this.mynum3.TabIndex = 20;
+            // 
+            // mynum4
+            // 
+            this.mynum4.Location = new System.Drawing.Point(180, 247);
+            this.mynum4.Name = "mynum4";
+            this.mynum4.Size = new System.Drawing.Size(50, 20);
+            this.mynum4.TabIndex = 21;
+            // 
+            // mynum5
+            // 
+            this.mynum5.Location = new System.Drawing.Point(236, 247);
+            this.mynum5.Name = "mynum5";
+            this.mynum5.Size = new System.Drawing.Size(50, 20);
+            this.mynum5.TabIndex = 22;
+            // 
+            // mynum6
+            // 
+            this.mynum6.Location = new System.Drawing.Point(292, 247);
+            this.mynum6.Name = "mynum6";
+            this.mynum6.Size = new System.Drawing.Size(50, 20);
+            this.mynum6.TabIndex = 23;
+            // 
+            // mynum7
+            // 
+            this.mynum7.Location = new System.Drawing.Point(348, 247);
+            this.mynum7.Name = "mynum7";
+            this.mynum7.Size = new System.Drawing.Size(50, 20);
+            this.mynum7.TabIndex = 24;
+            // 
+            // mynum8
+            // 
+            this.mynum8.Location = new System.Drawing.Point(436, 247);
+            this.mynum8.Name = "mynum8";
+            this.mynum8.Size = new System.Drawing.Size(50, 20);
+            this.mynum8.TabIndex = 25;
+            // 
+            // mynum9
+            // 
+            this.mynum9.Location = new System.Drawing.Point(492, 247);
+            this.mynum9.Name = "mynum9";
+            this.mynum9.Size = new System.Drawing.Size(50, 20);
+            this.mynum9.TabIndex = 26;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(208, 381);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(2);
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 27;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Tarkista
+            // 
+            this.Tarkista.Location = new System.Drawing.Point(180, 321);
+            this.Tarkista.Name = "Tarkista";
+            this.Tarkista.Size = new System.Drawing.Size(162, 23);
+            this.Tarkista.TabIndex = 28;
+            this.Tarkista.Text = "Tarkista";
+            this.Tarkista.UseVisualStyleBackColor = true;
+            this.Tarkista.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 450);
+            this.Controls.Add(this.Tarkista);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mynum9);
+            this.Controls.Add(this.mynum8);
+            this.Controls.Add(this.mynum7);
+            this.Controls.Add(this.mynum6);
+            this.Controls.Add(this.mynum5);
+            this.Controls.Add(this.mynum4);
+            this.Controls.Add(this.mynum3);
+            this.Controls.Add(this.mynum2);
+            this.Controls.Add(this.mynum1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label num1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox mynum1;
+        private System.Windows.Forms.TextBox mynum2;
+        private System.Windows.Forms.TextBox mynum3;
+        private System.Windows.Forms.TextBox mynum4;
+        private System.Windows.Forms.TextBox mynum5;
+        private System.Windows.Forms.TextBox mynum6;
+        private System.Windows.Forms.TextBox mynum7;
+        private System.Windows.Forms.TextBox mynum8;
+        private System.Windows.Forms.TextBox mynum9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Tarkista;
+    }
+}
+
---
+++ array lotto/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace array_lotto
+{
+    public partial class Form1 : Form
+    {
+		
+		Random m_random = new Random();
+
+		int[] selectedNumbers = new int[9];
+		
+		public Form1()
+        {
+            InitializeComponent();
+        }
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			// random number
+			for (int numberCount = 0; numberCount < selectedNumbers.Length; numberCount++)
+			{
+
+				int newNumber = m_random.Next(1, 40);
+
+				for (int i = 0; i < numberCount; i++)
+				{
+					if (newNumber == selectedNumbers[i])
+					{
+						// jos numero toistu
+						newNumber = m_random.Next(1, 40); 
+						continue; // Test again
+					}
+				}
+
+				selectedNumbers[numberCount] = newNumber;
+
+			}
+			Array.Sort(selectedNumbers);
+			num1.Text = string.Join(", ", selectedNumbers.Select(x => x.ToString()).ToArray());
+
+			
+
+
+		}
+
+
+
+
+
+
+
+
+			private void button2_Click(object sender, EventArgs e)
+			{
+				int oikeidenmaara = 0;
+				var veikkaus = new List<int>();
+				veikkaus.Add(Int32.Parse(mynum1.Text));
+				veikkaus.Add(Int32.Parse(mynum2.Text));
+				veikkaus.Add(Int32.Parse(mynum3.Text));
+				veikkaus.Add(Int32.Parse(mynum4.Text));
+				veikkaus.Add(Int32.Parse(mynum5.Text));
+				veikkaus.Add(Int32.Parse(mynum6.Text));
+				veikkaus.Add(Int32.Parse(mynum7.Text));
+				veikkaus.Add(Int32.Parse(mynum8.Text));
+				veikkaus.Add(Int32.Parse(mynum9.Text));
+
+				for (int i = 0; i < selectedNumbers.Length; i++)
+				{
+					for (int j = 0; j < veikkaus.Count; j++)
+					{
+						if (selectedNumbers[i] == veikkaus[j])
+						{
+							oikeidenmaara = oikeidenmaara + 1;
+						}
+					}
+
+				}
+
+				label1.Text = oikeidenmaara.ToString();
+
+				}
+	}
+
+}
